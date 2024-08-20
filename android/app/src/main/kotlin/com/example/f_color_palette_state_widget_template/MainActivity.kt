fa2fcd148b59cf259dfe24b1585494fd09c3772a
Augusto Salazar <augustosalazar@uniorte.edu.co>
2024-08-20 10:33:17 -0500

    update web and android

--- android/app/src/main/kotlin/com/example/f_color_palette_state_widget_template/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/f_color_palette_state_widget_template/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.f_color_palette_state_widget_template
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
